b70a0614fbadbdc112dc2930ab8b6a824d6b92a0
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-02-21 06:49:51 +0200

    deleted service-worker

--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 
-if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service worker registered:', registration);
-            })
-            .catch(error => {
-                console.log('Service worker registration failed:', error);
-            });
-    });
-}
-
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
