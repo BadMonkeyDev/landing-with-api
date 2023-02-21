b6e56ec10d8ca1d0d0979634c8097476c6d192b5
BadMonkeyDev <arsenii.aivazian@gmail.com>
2023-02-21 04:49:43 +0200

    fonts improvements and lazy loading for 2 sections

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ import {muiTheme} from "./muiTheme";
 
 import Header from "./components/sections/Header/Header";
 import HeroSection from "./components/sections/HeroSection/HeroSection";
-import UsersSection from "./components/sections/UsersSection/UsersSection";
-import RegistrationSection from "./components/sections/RegistrationSection/RegistrationSection";
+
+const  UsersSection =  React.lazy(() => import("./components/sections/UsersSection/UsersSection"));
+const RegistrationSection  = React.lazy(() => import("./components/sections/RegistrationSection/RegistrationSection"));
 
 function App() {
     const usersBlockRef = useRef<HTMLDivElement>(null);
